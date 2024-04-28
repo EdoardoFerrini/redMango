ac4ef8e8b5bab286655dc53d0c7b6032ea974940
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-04-28 11:17:34 +0200

    aggiunto pacchetto stripe e user nell acquisto

--- src/Components/Page/Cart/CartPickupDetails.tsx
+++ src/Components/Page/Cart/CartPickupDetails.tsx
@@ -11,12 +11,12 @@ function CartPickupDetails() {
   const shoppingCartFromStore: cartItemModel[] = useSelector(
     (state: RootState) => state.shoppingCartStore.cartItems ?? []
   );
-
+  const userData = useSelector((state: RootState) => state.userAuthStore)
   let grandTotal = 0;
   let totalItems = 0;
   const initialUserData = {
-    name: "",
-    email: "",
+    name: userData.fullName,
+    email: userData.email,
     phoneNumber: "",
   };
 
